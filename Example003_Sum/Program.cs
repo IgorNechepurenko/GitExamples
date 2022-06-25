03708f7fb722177287dcc0dc68bff31f2588e032
Igor Nechepurenko <nahalenok33@bk.ru>
2022-06-25 23:27:50 +0800

    treugolnik

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 31;
-int nemberB = 5;
+﻿int numberA = new Random().Next(1,10); // 1,2,3,4...
+Console.WriteLine(numberA);
+int nemberB = new Random().Next(1,10);
+Console.WriteLine(nemberB);
 int result = numberA + nemberB;
 Console.WriteLine(result);
