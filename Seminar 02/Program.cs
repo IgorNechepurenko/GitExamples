9e915e84b0b4c2cf3fcb26840231a783c93cf2dc
Igor Nechepurenko <nahalenok33@bk.ru>
2022-06-27 16:53:42 +0800

    zadachi seminara 2

---
+++ Seminar 02/Program.cs
@@ -0,0 +1,50 @@
+﻿// int n1, n2, n;
+// n = new Random().Next(0, 100);
+// Console.WriteLine(n);
+// n1 = n / 100;
+// n2 = n % 10;
+// Console.WriteLine(n1 * 10 + n3);
+
+// int n1 = Convert.ToInt32(Console.ReadLine());
+// int n2 = Convert.ToInt32(Console.ReadLine());
+// if(n1 % n2 == 0){
+//     Console.WriteLine("yes");
+// }
+// else{
+//     Console.WriteLine(n1 % n2);
+// }
+
+// int n;
+// n = Convert.ToInt32(Console.ReadLine());
+// if (n % 7 == 0){
+//     if (n % 23 == 0){
+//         Console.WriteLine("yes");
+//     }
+//     else{
+//         Console.WriteLine("no");
+//     }
+// }
+// else{
+//     Console.WriteLine("no");
+// }
+
+// int n;
+// n = Convert.ToInt32(Console.ReadLine());
+// if (n % 7 == 0 && n % 23 == 0){
+//     Console.WriteLine("yes");
+// }
+// else{
+//     Console.WriteLine("no");
+// }
+
+int a, b;
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+if (a * a == b){
+    Console.WriteLine("yes");
+}
+else{
+    Console.WriteLine("no");
+}
+
+
